4f15e8682ce40f36e9fd148a663d8e61e599f567
Cesar Murphy <cesar>
2019-04-01 22:40:31 +1100

    colour recognition for orange post-it cards

--- app.py
+++ app.py
@@ -1,10 +1,45 @@
+import cv2
+import numpy as np
 from flask import Flask, render_template, flash
 
 # Once you have built your image in Docker you can import OpenCV to use throughout the project.
 # import cv2
 
 app = Flask(__name__)
-app.secret_key = b'COOKIE_MONSTER' # Change this otherwise the other team might modify your cookies.
+# Change this otherwise the other team might modify your cookies.
+app.secret_key = b'COOKIE_MONSTER'
+
+# WORKING FOR GRAYSCALE OBJECT DETECTION
+# img = cv2.imread('cards.jpg', cv2.IMREAD_GRAYSCALE)
+# _, threshold = cv2.threshold(img, 240, 255, cv2.THRESH_BINARY)
+# contours, _ = cv2.findContours(
+#     threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+# cv2.drawContours(img, contours, -1, 0, 3)
+# cv2.imwrite('threshold.jpg', threshold)
+# cv2.imwrite('contours.jpg', img)
+
+# min_orange = np.array([111, 255, 255])
+# max_orange = np.array([91, 100, 100])
+# img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+
+image = cv2.imread('cards.jpg')
+hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+highlight = (255, 255, 0)
+
+min_orange = np.array([16, 100, 100], np.uint8)
+max_orange = np.array([30, 255, 255], np.uint8)
+
+mask = cv2.inRange(hsv, min_orange, max_orange)
+
+res = cv2.bitwise_and(image, image, mask=mask)
+
+contours, hierarchy = cv2.findContours(
+    mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+cv2.drawContours(image, contours, -1, highlight, 10)
+
+cv2.imwrite('contours.jpg', image)
 
 
 @app.route('/')
@@ -25,20 +60,18 @@ def upload():
 
 @app.route('/check_cv')
 def check_cv():
-    try: 
-        import cv2  
+    try:
+        import cv2
     except ModuleNotFoundError:
-        return render_template('detail.html', heading="OpenCV is NOT installed!", content=
-            """
+        return render_template('detail.html', heading="OpenCV is NOT installed!", content="""
                 If you are seeing this message it means you have not correctly spun up your Docker container.
                 <br>Refer to the instructions in the <a href="https://github.com/blakelockley/pace-2019-s1" target="_blank">README</a> file for more details.
             """)
 
-    return render_template('detail.html', heading="OpenCV is installed!", content=
-        """
+    return render_template('detail.html', heading="OpenCV is installed!", content="""
             You can use 'import cv2' in your flask app to access OpenCV.
         """)
 
-    
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
